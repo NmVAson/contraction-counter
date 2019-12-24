aa5f20ec20ca48d604cb4f64c6f87b9665e368bc
Giuliana Taylor <giulswatson@gmail.com>
2019-12-24 14:24:43 -0500

    Create alerter to alert when to call the doctor and when to head to the hospital

---
+++ services/Alerter.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {Alert} from 'react-native';
+
+const oneHourInMs = 3600000;
+
+export default class Alerter {
+    delta = 0.15;
+
+    trackContraction(frequency, duration) {
+        if(this.isTimeToCallDoc(frequency, duration)) {
+            this.triggerCallDoctorAlert();
+            setTimeout(() => Alert.alert('Time to head to the hospital!'), oneHourInMs);
+        }
+    }
+
+    isTimeToCallDoc(frequency, duration) {
+        let isLessThanFiveMinutesApart = frequency <= 5 + this.delta;
+        let isAboutAMinute = Math.abs(duration - 1) < this.delta;
+
+        return isLessThanFiveMinutesApart && isAboutAMinute;
+    }
+
+    triggerCallDoctorAlert() {
+        Alert.alert('Time to call your doctor! You\'ll be heading to the hospital in an hour!');
+    }
+
+    triggerHeadToHospitalAlert() {
+        Alert.alert('Time to head to the hospital!');
+    }
+}
---
+++ services/Alerter.spec.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {DeviceEventEmitter, Alert} from 'react-native';
+
+import Alerter from './Alerter';
+
+const oneHourInMs = 3600000;
+let alerter = new Alerter();
+let alertSpy = jest.spyOn(Alert, 'alert');
+
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+it('throws an alert when contractions are 5 minutes apart', () => {
+    let numberOfMinutesApart = 5;
+    let numberOfMinutesLong = 1;
+
+    alerter.trackContraction(numberOfMinutesApart, numberOfMinutesLong);
+
+    expect(alertSpy).toHaveBeenCalled();
+    expect(alertSpy).toHaveBeenCalledWith('Time to call your doctor! You\'ll be heading to the hospital in an hour!');
+});
+
+it('throws an alert when contractions are less than 5 minutes apart', () => {
+    let numberOfMinutesApart = 4;
+    let numberOfMinutesLong = 1;
+
+    alerter.trackContraction(numberOfMinutesApart, numberOfMinutesLong);
+
+    expect(alertSpy).toHaveBeenCalled();
+    expect(alertSpy).toHaveBeenCalledWith('Time to call your doctor! You\'ll be heading to the hospital in an hour!');
+});
+
+it('throws an alert when contractions are about 5 minutes apart', () => {
+    let delta = 0.1;
+    let numberOfMinutesApart = 5 + delta;
+    let numberOfMinutesLong = 1;
+
+    alerter.trackContraction(numberOfMinutesApart, numberOfMinutesLong);
+
+    expect(alertSpy).toHaveBeenCalled();
+    expect(alertSpy).toHaveBeenCalledWith('Time to call your doctor! You\'ll be heading to the hospital in an hour!');
+});
+
+it('does not throw an alert when contractions last longer than a minute', () => {
+    let numberOfMinutesApart = 5;
+    let numberOfMinutesLong = 1.16;
+
+    alerter.trackContraction(numberOfMinutesApart, numberOfMinutesLong);
+
+    expect(alertSpy).not.toHaveBeenCalled();
+});
+
+it('throws a second alert one hour after the contractions start about 5 minutes apart for about 1 minute long', () => {
+    let numberOfMinutesApart = 5;
+    let numberOfMinutesLong = 1;
+    alerter.trackContraction(numberOfMinutesApart, numberOfMinutesLong);
+    alertSpy.mockClear();
+
+    jest.runAllTimers();
+
+    expect(alertSpy).toHaveBeenCalled();
+    expect(alertSpy).toHaveBeenCalledWith('Time to head to the hospital!');
+});
